4e34f2824c6ed3b81550f663510bf73548276cc1
dugi <contact@tusk.world>
2019-06-13 14:34:18 +0200

    test

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+    await mongoose.connect(
+      "mongodb://localhost:27017/tusk_world" || process.env.MONGODB_URI,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
